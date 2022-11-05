8070351d095c4b4201e8e621c5ab29ec0a6b6d38
Leonardo <leo.vascon.dev@gmail.com>
2022-11-05 19:51:17 -0300

    exercicios corrigidos

--- aula5/src/Datas.java
+++ aula5/src/Datas.java
@@ -2,59 +2,29 @@ import java.util.Scanner;
 
 public class Datas {
     public static void main(String[] args) {
-        String data = "21 de julho de 2022";
-        System.out.println(data);
+        String data = "27/01/2022";
         converteData(data);
-
-        data = "27/10/2022";
-        System.out.println("valor de data modificado -> "+data);
+        data = "21 de julho de 2000";
         converteData(data);
     }
-    public static void converteData(String data){
-        //data = new Scanner(System.in).nextLine(); //caso queira inserir a data
+
+    public static void converteData(String data) {
+        String[] meses = {"", "janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
         String[] tokens;
-        if(data.length() == 10){
+        if (data.length() == 10) {
             tokens = data.split("/");
-            switch (tokens[1]) {
-            case "01" -> data = tokens[0] + " de janeiro de " + tokens[2];
-            case "02" -> data = tokens[0] + " de fevereiro de " + tokens[2];
-            case "03" -> data = tokens[0] + " de março de " + tokens[2];
-            case "04" -> data = tokens[0] + " de abril de " + tokens[2];
-            case "05" -> data = tokens[0] + " de maio de " + tokens[2];
-            case "06" -> data = tokens[0] + " de junho de " + tokens[2];
-            case "07" -> data = tokens[0] + " de julho de " + tokens[2];
-            case "08" -> data = tokens[0] + " de agosto de " + tokens[2];
-            case "09" -> data = tokens[0] + " de setembro de " + tokens[2];
-            case "10" -> data = tokens[0] + " de outubro de " + tokens[2];
-            case "11" -> data = tokens[0] + " de novembro de " + tokens[2];
-            case "12" -> data = tokens[0] + " de dezembro de " + tokens[2];
-            default -> {
-                System.out.println("data inválida, insira novamente: \n");
-                data = new Scanner(System.in).nextLine();
-            }
-        }
+
+            System.out.println(tokens[0] + " de " + meses[Integer.parseInt(tokens[1])] + " de " + tokens[2]);
         } else {
-            tokens = data.split(" de ");// reparando nos espaços para não dar erro no split;
-            switch (tokens[1]) {
-                case "janeiro" -> data = tokens[0] + "/01/" + tokens[2];
-                case "fevereiro" -> data = tokens[0] + "/02/" + tokens[2];
-                case "março" -> data = tokens[0] + "/03/" + tokens[2];
-                case "abril" -> data = tokens[0] + "/04/" + tokens[2];
-                case "maio" -> data = tokens[0] + "/05/" + tokens[2];
-                case "junho" -> data = tokens[0] + "/06/" + tokens[2];
-                case "julho" -> data = tokens[0] + "/07/" + tokens[2];
-                case "agosto" -> data = tokens[0] + "/08/" + tokens[2];
-                case "setembro" -> data = tokens[0] + "/09/" + tokens[2];
-                case "outubro" -> data = tokens[0] + "/10/" + tokens[2];
-                case "novembro" -> data = tokens[0] + "/11/" + tokens[2];
-                case "dezembro" -> data = tokens[0] + "/12/" + tokens[2];
-                default -> {
-                    System.out.println("data inválida, insira novamente: \n");
-                    data = new Scanner(System.in).nextLine();
+            tokens = data.split(" de ");
+            for (int i = 0; i < meses.length ; i++) {
+                if(meses[i].equals(tokens[1])){
+                    System.out.println(tokens[0]+"/"+ String.format("%02d",i) +"/"+tokens[2] );
                 }
             }
 
         }
-        System.out.println(data);
+
     }
 }
+
--- aula5/src/Morse.java
+++ aula5/src/Morse.java
@@ -4,55 +4,25 @@ public class Morse {
     public static void main(String[] args) {
         conversorMorse();
     }
-    public static void conversorMorse(){
+
+    public static void conversorMorse() {
+        String[] morseArray = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "/"};
+        String[] arrayLetras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y","Z",  "1", "2", "3", "4", "5", "6", "7", "8", "9","0"," "};
 
         StringBuilder output = new StringBuilder();
+
         System.out.println("Insira o código morse: ");
         String input = new Scanner(System.in).nextLine();
+
         String[] tokens = input.split(" ");
-        for (String token : tokens) {
-            switch (token) {
-                case ".-" -> output.append("A");
-                case "-..." -> output.append("B");
-                case "-.-." -> output.append("C");
-                case "-.." -> output.append("D");
-                case "." -> output.append("E");
-                case "..-." -> output.append("F");
-                case "--." -> output.append("G");
-                case "...." -> output.append("H");
-                case ".." -> output.append("I");
-                case ".---" -> output.append("J");
-                case "-.-" -> output.append("K");
-                case ".-.." -> output.append("L");
-                case "--" -> output.append("M");
-                case "-." -> output.append("N");
-                case "---" -> output.append("O");
-                case ".--." -> output.append("P");
-                case "--.-" -> output.append("Q");
-                case ".-." -> output.append("R");
-                case "..." -> output.append("S");
-                case "-" -> output.append("T");
-                case "..-" -> output.append("U");
-                case "...-" -> output.append("V");
-                case ".--" -> output.append("W");
-                case "-..-" -> output.append("X");
-                case "-.--" -> output.append("Y");
-                case "--.." -> output.append("Z");
-                case ".----" -> output.append("1");
-                case "..---" -> output.append("2");
-                case "...--" -> output.append("3");
-                case "....-" -> output.append("4");
-                case "....." -> output.append("5");
-                case "-...." -> output.append("6");
-                case "--..." -> output.append("7");
-                case "---.." -> output.append("8");
-                case "----." -> output.append("9");
-                case "-----" -> output.append("0");
-                case "/" -> output.append(" ");
-                default -> System.out.println("entrada inválida");
-
+        for (String token: tokens) {
+            for(int i = 0; i< morseArray.length; i++){
+                if (morseArray[i].equals(token)){
+                    output.append(arrayLetras[i]);
+                }
             }
+        }
 
-        } System.out.println(output);
+        System.out.println(output);
     }
 }
